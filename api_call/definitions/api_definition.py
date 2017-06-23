2cb96a4a93e9e789c96266d5e80183665eb0b9f8
pein <cetazo@gmail.com>
2017-06-23 14:31:39 +0600

    Updated api definiton object.
    Signed-off-by: pein <cetazo@gmail.com>

--- api_call/definitions/api_definition.py
+++ api_call/definitions/api_definition.py
@@ -1,8 +1,8 @@
 api_list = [
     {
-        "id" : 1,
+        "api_id" : 1,
         "api_partial_path" : "/commons/tenant/:hash_string/",
-        "parent_id": -1,
+        "parent_api_id": -1,
         "method" : "GET",
         "context_data" : {
             "hash_string" : "$HASH_STRING"
